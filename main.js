62d656450fd71650fe349c1c140a543775f26cdc
johnthairukamau <tkamau82@gmail.com>
2023-03-28 12:41:27 +0300

    Delete function

--- main.js
+++ main.js
@@ -1,23 +1,7 @@
-fetch("http://localhost:3000/Shoes")
-  .then(function (response) {
-    console.log(response);
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  });
-
-card.querySelector('#delete').addEventListener('click', () => {
-    card.remove()
-    deleteShoe(shoe.id)
-})
-    
 function deleteShoe(id){
     fetch(`http://localhost:3000/Shoes/${id}` ,{
-        method:'DELETE',
-        headers: {
-            'Content-type':'application/json'
-        }
+        method:'DELETE'
+        
     })
     .then(res => res.json())
     .then(animal => console.log(shoe))
